79854732be0d285e14617759cc3a8e53e6ae2ac9
Zsiga Norbert <zsiganp@gmail.com>
2024-12-04 16:42:54 +0100

    órai 12-04

--- maui/01-SimpleMovieApp/Solution.Database/AppDbContext.cs
+++ maui/01-SimpleMovieApp/Solution.Database/AppDbContext.cs
@@ -2,6 +2,7 @@
 
 public class AppDbContext : DbContext
 {
+    public DbSet<MovieEntity> Movies { get; set; }
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     { 
     
